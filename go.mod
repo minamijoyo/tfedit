6889205a694fd787d31bde76ac4472649fe6de37
Masayuki Morita <minamijoyo@gmail.com>
2022-04-20 18:11:26 +0900

    Extract Migration as a type

--- go.mod
+++ go.mod
@@ -18,7 +18,7 @@ require (
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/hashicorp/go-version v1.3.0 // indirect
+	github.com/hashicorp/go-version v1.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
@@ -30,7 +30,7 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
--- migration/migration.go
+++ migration/migration.go
@@ -11,7 +11,19 @@ import (
 	_ "github.com/minamijoyo/tfedit/migration/schema/aws" // Register schema for aws
 )
 
-var migrationTemplate string = `migration "state" "awsv4upgrade" {
+// Migration is a type which is equivalent to tfmigrate.StateMigratorConfig of
+// minamijoyo/tfmigrate.
+// The current implementation doesn't encode migration actions to a file
+// directly with gohcl, so we avoid to depend on tfmigrate's type and we define
+// only what we need here.
+type Migration struct {
+	// Dir is a working directory for executing terraform command.
+	Dir string
+	// Actions is a list of state action.
+	Actions []string
+}
+
+var migrationTemplate = `migration "state" "awsv4upgrade" {
   actions = [
   {{- range . }}
     "{{ . }}",
@@ -20,28 +32,45 @@ var migrationTemplate string = `migration "state" "awsv4upgrade" {
 }
 `
 
+var compiledMigrationTemplate = template.Must(template.New("migration").Parse(migrationTemplate))
+
+// AppendAction appends an action to migration.
+func (m *Migration) AppendAction(action string) {
+	m.Actions = append(m.Actions, action)
+}
+
+// Encode converts a state migration config to bytes
+func (m *Migration) Encode() ([]byte, error) {
+	// Encoding StateMigratorConfig directly with gohcl has some problems.
+	// An array contains multiple elements is output as one line. It's not readable
+	// for multiple actions. In additon, the default value is set explicitly, it's
+	// not only redundant but also increases cognitive load for user who isn't
+	// familiar with tfmigrate.
+	// So we use text/template to render a migration file.
+	var output bytes.Buffer
+	if err := compiledMigrationTemplate.Execute(&output, m.Actions); err != nil {
+		return nil, fmt.Errorf("failed to render migration file: %s", err)
+	}
+
+	return output.Bytes(), nil
+}
+
 func Generate(planJSON []byte) ([]byte, error) {
 	var plan tfjson.Plan
 	if err := json.Unmarshal(planJSON, &plan); err != nil {
 		return nil, fmt.Errorf("failed to parse plan file: %s", err)
 	}
 
-	var migrateActions []string
+	var migration Migration
 	for _, rc := range plan.ResourceChanges {
 		if rc.Change.Actions.Create() {
 			address := rc.Address
 			after := rc.Change.After.(map[string]interface{})
 			importID := schema.ImportID(rc.Type, after)
 			migrateAction := fmt.Sprintf("import %s %s", address, importID)
-			migrateActions = append(migrateActions, migrateAction)
+			migration.AppendAction(migrateAction)
 		}
 	}
 
-	tpl := template.Must(template.New("migration").Parse(migrationTemplate))
-	var output bytes.Buffer
-	if err := tpl.Execute(&output, migrateActions); err != nil {
-		return nil, fmt.Errorf("failed to render migration file: %s", err)
-	}
-
-	return output.Bytes(), nil
+	return migration.Encode()
 }
