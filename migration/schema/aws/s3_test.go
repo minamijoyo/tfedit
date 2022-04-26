252b08dedf933166aa423b5130c5a1200ed69b9b
Masayuki Morita <minamijoyo@gmail.com>
2022-04-26 18:01:33 +0900

    Use a raw JSON string as a test data for easy testing

--- migration/schema/aws/s3_test.go
+++ migration/schema/aws/s3_test.go
@@ -1,6 +1,7 @@
 package aws
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/minamijoyo/tfedit/migration/schema"
@@ -8,24 +9,31 @@ import (
 
 func TestImportIDFuncAWSS3BucketACL(t *testing.T) {
 	cases := []struct {
-		desc string
-		r    schema.Resource
-		want string
+		desc     string
+		resource string
+		want     string
 	}{
 		{
 			desc: "acl",
-			r: map[string]interface{}{
-				"acl":                   "private",
-				"bucket":                "tfedit-test",
-				"expected_bucket_owner": nil,
-			},
+			resource: `
+{
+  "acl": "private",
+  "bucket": "tfedit-test",
+  "expected_bucket_owner": null
+}
+`,
 			want: "tfedit-test,private",
 		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.desc, func(t *testing.T) {
-			got := importIDFuncAWSS3BucketACL(tc.r)
+			var r schema.Resource
+			if err := json.Unmarshal([]byte(tc.resource), &r); err != nil {
+				t.Fatalf("failed to unmarshal json: %s", err)
+			}
+
+			got := importIDFuncAWSS3BucketACL(r)
 
 			if got != tc.want {
 				t.Errorf("got = %s, but want = %s", got, tc.want)
