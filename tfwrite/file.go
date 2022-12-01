c0cc48d6b1d03f15f7b7538ef0977d059ab5cef4
Masayuki Morita <minamijoyo@gmail.com>
2022-12-01 11:24:41 +0900

    Generalize block finders

--- tfwrite/file.go
+++ tfwrite/file.go
@@ -23,73 +23,91 @@ func (f *File) Raw() *hclwrite.File {
 	return f.raw
 }
 
-// FindBlocksByType returns all matching blocks from the body that have the
-// given blockType and schemaType or returns an empty list if not found.
-// If the given schemaType is a non-empty string, filter the results.
-func (f *File) FindBlocksByType(blockType string, schemaType string) []Block {
+// parseBlock is a factory method for Block.
+func parseBlock(block *hclwrite.Block) Block {
+	switch block.Type() {
+	case "resource":
+		return NewResource(block)
+	case "data":
+		return NewDataSource(block)
+	case "provider":
+		return NewProvider(block)
+	default:
+		return newBlock(block) // unknown
+	}
+}
+
+// Blocks returns all blocks.
+func (f *File) Blocks() []Block {
 	var blocks []Block
 
 	for _, block := range f.Raw().Body().Blocks() {
-		if block.Type() == blockType {
-			var b Block
-			switch blockType {
-			case "resource":
-				b = NewResource(block)
-			case "data":
-				b = NewDataSource(block)
-			case "provider":
-				b = NewProvider(block)
-			default:
-				continue
-			}
-
-			if schemaType != "" && schemaType != b.SchemaType() {
-				continue
-			}
-			blocks = append(blocks, b)
-		}
+		b := parseBlock(block)
+		blocks = append(blocks, b)
 	}
 
 	return blocks
 }
 
-// FindResourcesByType returns all matching resources from the body that have the
-// given schemaType or returns an empty list if not found.
-func (f *File) FindResourcesByType(schemaType string) []*Resource {
-	var matched []*Resource
+// FindBlocksByType returns all matching blocks from the body that have the
+// given blockType and schemaType or returns an empty list if not found.
+// If the given schemaType is a non-empty string, filter the results.
+func (f *File) FindBlocksByType(blockType string, schemaType string) []Block {
+	var matched []Block
 
-	for _, block := range f.FindBlocksByType("resource", schemaType) {
-		b := block.(*Resource)
+	for _, b := range f.Blocks() {
+		if blockType != "" && blockType != b.Type() {
+			continue
+		}
+		if schemaType != "" && schemaType != b.SchemaType() {
+			continue
+		}
 		matched = append(matched, b)
 	}
 
 	return matched
+}
+
+// findAnyBlockByType is a generic helper method for implementing block finders.
+// It returns all matching blocks from the body that have the given type
+// parameter and schemaType or returns an empty list if not found.
+// If the given schemaType is a non-empty string, filter the results.
+// Note that the Generics doesn't allow an instance method to have a type parameter.
+func findAnyBlockByType[T Block](f *File, schemaType string) []T {
+	var matched []T
+
+	for _, block := range f.Blocks() {
+		b, ok := block.(T)
+		if !ok {
+			// ignore other block types
+			continue
+		}
+
+		if schemaType != "" && schemaType != b.SchemaType() {
+			continue
+		}
+		matched = append(matched, b)
+	}
+
+	return matched
+}
+
+// FindResourcesByType returns all matching resources from the body that have the
+// given schemaType or returns an empty list if not found.
+func (f *File) FindResourcesByType(schemaType string) []*Resource {
+	return findAnyBlockByType[*Resource](f, schemaType)
 }
 
 // FindDataSourcesByType returns all matching data sources from the body that have the
 // given schemaType or returns an empty list if not found.
 func (f *File) FindDataSourcesByType(schemaType string) []*DataSource {
-	var matched []*DataSource
-
-	for _, block := range f.FindBlocksByType("data", schemaType) {
-		b := block.(*DataSource)
-		matched = append(matched, b)
-	}
-
-	return matched
+	return findAnyBlockByType[*DataSource](f, schemaType)
 }
 
 // FindProvidersByType returns all matching providers from the body that have the
 // given schemaType or returns an empty list if not found.
 func (f *File) FindProvidersByType(schemaType string) []*Provider {
-	var matched []*Provider
-
-	for _, block := range f.FindBlocksByType("provider", schemaType) {
-		b := block.(*Provider)
-		matched = append(matched, b)
-	}
-
-	return matched
+	return findAnyBlockByType[*Provider](f, schemaType)
 }
 
 // appendBlock appends a given block to the file.
