e647b736e16c984dfc518fb88ba5199a7bd4bcfc
Masayuki Morita <minamijoyo@gmail.com>
2022-12-28 18:32:52 +0900

    Implement rename references in an attribute

--- tfwrite/attribute.go
+++ tfwrite/attribute.go
@@ -1,6 +1,8 @@
 package tfwrite
 
 import (
+	"strings"
+
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/minamijoyo/hcledit/editor"
 )
@@ -23,4 +25,14 @@ func (a *Attribute) ValueAsString() (string, error) {
 // ValueAsTokens returns a value of Attribute as raw tokens.
 func (a *Attribute) ValueAsTokens() hclwrite.Tokens {
 	return a.raw.Expr().BuildTokens(nil)
+}
+
+// RenameReference renames all variable references contained in the value.
+// The `from` and `to` arguments are specified as dot-delimited resource addresses.
+// The `from` argument can be a partial prefix match, but must match the length
+// of the `to` argument.
+func (a *Attribute) RenameReference(from string, to string) {
+	search := strings.Split(from, ".")
+	replacement := strings.Split(to, ".")
+	a.raw.Expr().RenameVariablePrefix(search, replacement)
 }
--- tfwrite/attribute_test.go
+++ tfwrite/attribute_test.go
@@ -80,3 +80,90 @@ foo {
 		})
 	}
 }
+
+func TestAttributeRenameReference(t *testing.T) {
+	cases := []struct {
+		desc string
+		src  string
+		from string
+		to   string
+		want string
+		ok   bool
+	}{
+		{
+			desc: "simple",
+			src: `
+foo {
+  bar = aaa.bbb.ccc
+}
+`,
+			from: "aaa.bbb.ccc",
+			to:   "xxx.yyy.zzz",
+			want: ` xxx.yyy.zzz`,
+			ok:   true,
+		},
+		{
+			desc: "prefix",
+			src: `
+foo {
+  bar = aaa.bbb.ccc
+}
+`,
+			from: "aaa.bbb",
+			to:   "xxx.yyy",
+			want: ` xxx.yyy.ccc`,
+			ok:   true,
+		},
+		{
+			desc: "string template",
+			src: `
+foo {
+  bar = "AAA-${aaa.bbb.ccc}-ZZZ"
+}
+`,
+			from: "aaa.bbb",
+			to:   "xxx.yyy",
+			want: ` "AAA-${xxx.yyy.ccc}-ZZZ"`,
+			ok:   true,
+		},
+		{
+			desc: "multiple references in a string template",
+			src: `
+foo {
+  bar = "AAA-${aaa.bbb.ccc}-ZZZ-${aaa.bbb.ccc}"
+}
+`,
+			from: "aaa.bbb",
+			to:   "xxx.yyy",
+			want: ` "AAA-${xxx.yyy.ccc}-ZZZ-${xxx.yyy.ccc}"`,
+			ok:   true,
+		},
+		{
+			desc: "not found",
+			src: `
+foo {
+  bar = aaa.bbb.ccc
+}
+`,
+			from: "AAA.BBB.CCC",
+			to:   "xxx.yyy.zzz",
+			want: ` aaa.bbb.ccc`,
+			ok:   true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			f := parseTestFile(t, tc.src)
+			b := findFirstTestBlock(t, f)
+			attr := b.GetAttribute("bar")
+			attr.RenameReference(tc.from, tc.to)
+			tokens := attr.ValueAsTokens()
+			got := string(tokens.Bytes())
+
+			if got != tc.want {
+				t.Errorf("got = %s, but want = %s", got, tc.want)
+			}
+		})
+	}
+}
